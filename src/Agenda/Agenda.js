b65a928e8cbc5efc00e76f1d4e0ae63307625541
Jerald George <jjjera>
2019-03-02 15:25:08 +0530

    final commit

--- src/Agenda/Agenda.js
+++ src/Agenda/Agenda.js
@@ -6,7 +6,6 @@ import {
   TouchableOpacity
 } from 'react-native';
 import {Agenda} from 'react-native-calendars';
-import InputBox from '../InputBox/InputBox';
 import DialogInput from 'react-native-dialog-input';
 
 class AgendaScreen extends Component {
@@ -16,40 +15,85 @@ class AgendaScreen extends Component {
       items: {},
       isDialogVisible: false,
       day: {timestamp : new Date()}
-    };
-  }
+      };
+    }
 
-  showDialog(isShow,day){
-    console.log('showDialog called');
-    this.setState({isDialogVisible: isShow,day:day});
-  }
-
-  closeDialogBox(){
-    console.log('closeDialogBox called');
-    this.setState({isDialogVisible: false});
-  }
-
-  sendInput(inputText){
-    // console.log("sendInput (DialogInput#1): " + inputText);
-    // console.log('day is',this.state.day);
-    this.loadItems(inputText);
+      showDialog(isShow,day){
+        // console.log('showDialog called');
+        this.setState({isDialogVisible: isShow,day:day});
       }
 
+      closeDialogBox(){
+        // console.log('closeDialogBox called');
+        this.setState({isDialogVisible: false});
+      }
+
+      sendInput(inputText){
+        // console.log('sendInput  called');
+        // console.log("sendInput (DialogInput#1): " + inputText);
+        // console.log('day is',this.state.day);
+        this.loadItems(inputText);
+      }
+
+      loadItems(inputText) {
+        // console.log('loadItems called',inputText);
+        // console.log('day is',this.state.day);
+        let timestamp = this.state.day.timestamp;
+        // console.log('timestamp is',timestamp);
+        const time = timestamp;
+        // console.log('time is',time);
+        const strTime = this.timeToString(time);
+        // console.log('strTime called',strTime);
+          this.state.items[strTime] = [];
+          // console.log('state items',this.state.items[strTime]);
+            this.state.items[strTime].push({
+              name: inputText,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+            console.log('state.items',this.state.items[strTime]);
+            const newItems = {};
+            Object.keys(this.state.items)
+              .forEach(key => {newItems[key] = this.state.items[key];});
+              this.setState({
+                items: newItems
+              });
+            // console.log('new items',newItems);
+              this.closeDialogBox();
+            }
+
+            timeToString(time) {
+              console.log('timeToString called',time);
+              const date = new Date(time);
+              return date.toISOString().split('T')[0];
+          }
+
+          renderItem(item) {
+            console.log('renderItem called',item);
+            return (
+              <View style={[styles.item, {height: item.height}]}>
+                <Text>{item.name}</Text>
+              </View>
+            );
+          }
+
+        rowHasChanged(r1, r2) {
+          console.log('rowHasChanged called');
+          return r1.name !== r2.name;
+        }
+
   render() {
-    console.log('state is',this.state.day);
+    // console.log('state is',this.state.day);
     return (
       <View style={{flex:1}}>
       <Agenda
         items={this.state.items}
-        loadItemsForMonth={this.loadItems.bind(this)}
-        selected={'2019-02-28'}
         renderItem={this.renderItem.bind(this)}
-        renderEmptyDate={this.renderEmptyDate.bind(this)}
         rowHasChanged={this.rowHasChanged.bind(this)}
         onDayPress={(day)=>{
-          console.log('onDayPress called',day);
-          this.showDialog(true,day);
+        this.showDialog(true,day);
         }}
+
+  onCalendarToggled={(calendarOpened) => {console.log(calendarOpened)}}
       />
         <DialogInput isDialogVisible={this.state.isDialogVisible}
           title={"DialogInput 1"}
@@ -58,58 +102,8 @@ class AgendaScreen extends Component {
           submitInput={ (inputText) => {this.sendInput(inputText)} }
           closeDialog={ () => {this.closeDialogBox(false)}}>
         </DialogInput>
-        <TouchableOpacity onPress={()=>{this.showDialog(true)}} style={{padding:10}}>
-          <Text>Show DialogInput #1</Text>
-        </TouchableOpacity>
       </View>
     );
-  }
-
-  loadItems(inputText) {
-    console.log('loadItems called',inputText);
-    setTimeout(() => {
-      console.log('setTimeout called');
-      let timestamp = this.state.day.timestamp;
-      const time = timestamp + 60 * 24 * 60 * 60 * 1000;
-      console.log('time is',time);
-      const strTime = this.timeToString(time);
-      console.log('strTime called',strTime);
-        this.state.items[strTime] = [];
-        console.log('state items',this.state.items[strTime]);
-          this.state.items[strTime].push({
-            name: 'Event is: ' + inputText,
-            height: Math.max(50, Math.floor(Math.random() * 150))
-          });
-          console.log('state.items',this.state.items);
-          const newItems = {};
-          Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
-          this.setState({
-            items: newItems
-          });
-          console.log('new items',newItems);
-    },1000);
-    this.closeDialogBox();
-  }
-
-  timeToString(time) {
-    const date = new Date(time);
-    return date.toISOString().split('T')[0];
-  }
-
-  renderItem(item) {
-    return (
-      <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
-    );
-  }
-
-  renderEmptyDate() {
-    return (
-      <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
-    );
-  }
-
-  rowHasChanged(r1, r2) {
-    return r1.name !== r2.name;
   }
 
 }
